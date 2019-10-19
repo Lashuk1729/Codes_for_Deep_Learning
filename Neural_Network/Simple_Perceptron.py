07ab0926149e80f2b9e4ff906081ca9f8cd91759
Lashuk1729 <kushalborkar.11@gmail.com>
2019-10-19 12:32:57 +0530

    created perceptrons for the NOT operators

--- Neural_Network/Simple_Perceptron.py
+++ Neural_Network/Simple_Perceptron.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def perceptron_AND(test_input, weights, bais):
+
+def perceptron_AND(test_input, weights, bais, correct_outputs):
 
     outputs = []
     # Generate and check output
@@ -23,7 +24,8 @@ def perceptron_AND(test_input, weights, bais):
 
     return output
 
-def perceptron_OR(test_input, weights, bais):
+
+def perceptron_OR(test_input, weights, bais, correct_outputs):
 
     outputs = []
     # Generate and check output
@@ -42,6 +44,31 @@ def perceptron_OR(test_input, weights, bais):
     else:
         print('We got {} wrong.  Keep trying!\n'.format(num_wrong))
     print(output_frame.to_string(index=False))
+    print('\n\n')
+
+    return output
+
+
+def perceptron_NOT(test_input, weights, bais, correct_outputs):
+
+    outputs = []
+    # Generate and check output
+    for test_input, correct_output in zip(test_inputs, correct_outputs):
+        linear_combination = weights * test_input + bias
+        output = int(linear_combination >= 0)
+        is_correct_string = 'Yes' if output == correct_output else 'No'
+        outputs.append([test_input, linear_combination, output, is_correct_string])
+
+    # Print output
+    num_wrong = len([output[4] for output in outputs if output[3] == 'No'])
+    output_frame = pd.DataFrame(outputs, columns=[
+                                'Input', '  Linear Combination', '  Activation Output', '  Is Correct'])
+    print('Output for NOT Operation:')
+    if not num_wrong:
+        print('Nice!  We got it all correct.\n')
+    else:
+        print('We got {} wrong.  Keep trying!\n'.format(num_wrong))
+    print(output_frame.to_string(index=False))
 
     return output
 
@@ -54,7 +81,7 @@ weights = [0.5, 0.5]
 bias = -1
 
 # Output from perceptron_AND
-outputs = perceptron_AND(test_inputs, weights, bias)
+outputs = perceptron_AND(test_inputs, weights, bias, correct_outputs)
 
 
 # Manually setting: weight1, weight2, and bias for OR Perceptron
@@ -63,4 +90,15 @@ weights = [0.5, 0.5]
 bias = -0.5
 
 # Output from perceptron_OR
-outputs = perceptron_OR(test_inputs, weights, bias)
+outputs = perceptron_OR(test_inputs, weights, bias, correct_outputs)
+
+# Inputs and outputs for AND/OR Operation
+test_inputs = [0, 1]
+
+# Manually setting: weight1, weight2, and bias for AND Perceptron
+correct_outputs = [True, False]
+weights = -2
+bias = 1
+
+# Output from perceptron_NOT
+outputs = perceptron_NOT(test_inputs, weights, bias, correct_outputs)
